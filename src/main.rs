951380c60360872ac8c74089dcd1c8b923b1be4d
Nobody <nobody@localhost>
2022-10-03 12:47:58 +0000

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,36 @@
+// for #[expect(...)]
+#![feature(lint_reasons)]
+
+use clap::Parser;
+use mimalloc::MiMalloc;
+use tracing::info;
+use tracing_subscriber::EnvFilter;
+
+#[global_allocator]
+static GLOBAL: MiMalloc = MiMalloc;
+
+#[derive(Parser, Debug)]
+#[clap(name = "template", version)]
+/// template
+enum TemplateCommand {
+    /// Do something
+    #[clap(name = "something")]
+    Something {}
+}
+
+fn main() {
+    let env_filter = EnvFilter::try_from_default_env()
+        .or_else(|_| EnvFilter::try_new("warn"))
+        .unwrap();
+    tracing_subscriber::fmt()
+        .with_writer(std::io::stderr)
+        .with_env_filter(env_filter)
+        .init();
+
+    let command = TemplateCommand::parse();
+    match command {
+        TemplateCommand::Something {} => {
+            info!("something");
+        }
+    }
+}
